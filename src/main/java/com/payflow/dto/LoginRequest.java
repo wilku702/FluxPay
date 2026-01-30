1829dd901d01bdd3d5210672d1643d309d67098b
wilku702 <willkung702@gmail.com>
2026-01-30 09:12:00 -0600

    Add LoginRequest DTO with validation

---
+++ src/main/java/com/payflow/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.payflow.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
