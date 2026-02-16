6735b215f98d718cd4fd87787f1dd6a97ea7cf65
wilku702 <willkung702@gmail.com>
2026-02-16 09:00:00 -0600

    Add max password length (72) for bcrypt compatibility

--- src/main/java/com/payflow/dto/RegisterRequest.java
+++ src/main/java/com/payflow/dto/RegisterRequest.java
@@ -19,7 +19,7 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Size(min = 8, max = 72, message = "Password must be between 8 and 72 characters")
     private String password;
 
     @NotBlank(message = "Full name is required")
