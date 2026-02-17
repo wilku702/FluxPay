4bbf136cbb03e19bb17b1ff49d8bcdcb5abed58e
wilku702 <willkung702@gmail.com>
2026-02-17 10:08:00 -0600

    Add equals/hashCode based on id to Account entity

--- src/main/java/com/payflow/model/Account.java
+++ src/main/java/com/payflow/model/Account.java
@@ -1,6 +1,7 @@
 package com.payflow.model;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Account {
 
     @Id
